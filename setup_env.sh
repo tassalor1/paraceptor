a8d0262d39f1f27fec3fd74c5afc789adfc61b4f
tassalor1 <walkerconnor14@gmail.com>
2024-11-16 08:17:42 +0000

    setup env file

---
+++ setup_env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Activate the Conda environment
+conda activate ros_env
+
+# Source ROS setup.bash
+source /opt/ros/humble/setup.bash
+
+# Source your workspace's setup.bash
+source ~/paraceptor/install/setup.bash
+
+# Any additional setup commands can be added here
